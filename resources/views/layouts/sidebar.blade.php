0291bf2a5740b3556812efea673ba9d29a916d1d
Sadin21 <emdeasaddin21@gmail.com>
2024-03-06 20:05:02 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,48 @@
+<div class="text-decoration-none text-body position-relative mb-1 d-flex align-items-center gap-2 user-select-none cursor-pointer mt-3 active" href="">
+    <div class="fw-medium">Menu</div>
+</div>
+
+<a class="sidebar-menu text-decoration-none text-body position-relative mb-1 d-flex align-items-center gap-2 user-select-none cursor-pointer " href="">
+    <div class="col-2 d-flex align-items-center">
+        <ion-icon name="grid-outline" class="font-icon"></ion-icon>
+    </div>
+    <div class="col">
+        <div class="fw-small">Dashboard</div>
+    </div>
+</a>
+
+{{-- <div class="sidebar-menu position-relative mb-1 d-flex align-items-center justify-content-between gap-2 user-select-none cursor-pointer" data-bs-toggle="collapse" data-bs-target="#finance-item" aria-expanded="true" aria-controls="finance-item">
+    <div class="d-flex align-items-center gap-2">
+            <ion-icon name="wallet-outline" class="font-icon me-3"></ion-icon>
+            <div class="fw-small">Finance</div>
+        </div>
+        <ion-icon name="chevron-up" class="f20"></ion-icon>
+
+</div>
+<div class="collapse show ps-4" id="finance-item">
+    <a class="sidebar-menu sidebar-text text-decoration-none text-body d-block mb-1 user-select-none cursor-pointer {{ str_contains(Route::currentRouteName(), 'withdraw.pendapatan')? 'active' : '' }}" href="{{ route('withdraw.pendapatan') }}">
+        Pendapatan
+    </a>
+
+    <a class="sidebar-menu sidebar-text text-decoration-none text-body d-block mb-1 user-select-none cursor-pointer {{ str_contains(Route::currentRouteName(), 'withdraw.pencairan')? 'active' : '' }}" href="{{ route('withdraw.pencairan') }}">
+        Pencairan
+    </a>
+</div>
+
+<a class="sidebar-menu text-decoration-none text-body position-relative mb-1 d-flex align-items-center gap-2 user-select-none cursor-pointer {{ str_contains(Route::currentRouteName(), 'delivery.index')? 'active' : '' }}" href="{{ route('delivery.index') }}">
+    <div class="col-2 d-flex align-items-center">
+        <ion-icon name="clipboard-outline" class="font-icon"></ion-icon>
+    </div>
+    <div class="col">
+        <div class="fw-small">Delivery Order</div>
+    </div>
+</a>
+
+<a class="sidebar-menu text-decoration-none text-body position-relative mb-1 d-flex align-items-center gap-2 user-select-none cursor-pointer {{ str_contains(Route::currentRouteName(), 'fleet.index')? 'active' : '' }}" href="{{ route('fleet.index') }}">
+    <div class="col-2 d-flex align-items-center">
+        <ion-icon name="bus-outline" class="font-icon"></ion-icon>
+    </div>
+    <div class="col">
+        <div class="fw-small">Armada</div>
+    </div>
+</a> --}}
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,192 @@
+@extends('layouts.app')
+
+@section('content')
+<section>
+    <div class="position-relative">
+        <img src="{{ url('/assets/imgs/bg2.png') }}" alt="" style="height: 600px; background-size: auto; border-radius: 10px;" class="w-100">
+        <div class="position-absolute top-50 start-50 translate-middle text-center">
+            <h1 class="poppins-semibold text-white">Find A Product</h1>
+            <h1 class="poppins-semibold text-white">You'll Love To Live</h1>
+            <div class="bg-white mt-4 p-2 d-flex gap-3" style="width: 600px; border-radius: 4px;">
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Cari kategori produ</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <button type="button" class="btn poppins-medium text-white fs-14 py-2" style="background-color: #00A3A8;">Cari</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="" style="margin-top: 5rem;">
+    <div class="d-flex justify-content-between">
+        <div>
+            <h3 class="poppins-semibold dark-green">Produk Terbaru</h3>
+            <h6 class="poppins-medium fs-14">This week splecial products from Partners</h6>
+        </div>
+        <div>
+            <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Detail</a>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/chair.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Kursi Hijau</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Harga sewa mulai dari</p>
+                        <p class="mb-0 poppins-medium fs-14">Rp. 500.000</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/chair.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Kursi Hijau</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Harga sewa mulai dari</p>
+                        <p class="mb-0 poppins-medium fs-14">Rp. 500.000</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/chair.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Kursi Hijau</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Harga sewa mulai dari</p>
+                        <p class="mb-0 poppins-medium fs-14">Rp. 500.000</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Detail</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/chair.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Kursi Hijau</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Harga sewa mulai dari</p>
+                        <p class="mb-0 poppins-medium fs-14">Rp. 500.000</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Detail</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="" style="margin-top: 5rem;">
+    <div class="d-flex justify-content-between">
+        <div>
+            <h3 class="poppins-semibold dark-green">Produk Kategori</h3>
+            <h6 class="poppins-medium fs-14">This week splecial products from Partners</h6>
+        </div>
+        <div>
+            {{-- <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Detail</a> --}}
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/laptop.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Elektronik</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Total barang tersedia saat ini</p>
+                        <p class="mb-0 poppins-medium fs-14">23 Unit</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Produk</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/laptop.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Elektronik</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Total barang tersedia saat ini</p>
+                        <p class="mb-0 poppins-medium fs-14">23 Unit</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Produk</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/laptop.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Elektronik</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Total barang tersedia saat ini</p>
+                        <p class="mb-0 poppins-medium fs-14">23 Unit</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Produk</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="{{ url('/assets/imgs/laptop.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Elektronik</h5>
+                    <div class="my-3">
+                        <p class="mb-0 poppins-regular fs-12">Total barang tersedia saat ini</p>
+                        <p class="mb-0 poppins-medium fs-14">23 Unit</p>
+                    </div>
+                    <a href="#" class="btn poppins-medium text-white fs-14" style="background-color: #184A4B;">Lihat Produk</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="" style="margin-top: 5rem;">
+    <div class="d-flex justify-content-center">
+        <div class="w-50">
+            <div class="text-center">
+                {{-- <h3 class="poppins-semibold dark-green">Tentang Kami</h3> --}}
+                <h5 class="poppins-regular mt-4"><span class="poppins-semibold dark-green">Zewa</span> merupakan platform atau wadah digital yang mempertemukan mitra sewa yang ingin menyewakan barangnya dengan penyewa barang yang membutuhkan suatu barang untuk disewa dengan Lebih Mudah, Lebih Aman, dan Lebih Terpercaya</h5>
+            </div>
+        </div>
+    </div>
+    <img src="{{ url('/assets/imgs/company.jpg') }}" alt="" style="height: 600px; background-size: auto; border-radius: 10px;" class="w-100">
+    <div class="row mt-4">
+        <div class="col">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Visi</h5>
+                    <p class="card-text poppins-light fs-14">Menjadi platform yang aman dan terpercaya dalam sewa menyewa barang</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title poppins-medium fs-18">Visi</h5>
+                    <ul>
+                        <li class="poppins-light fs-14">Memberikan pelayanan terbaik kepada mitra & pelanggan.</li>
+                        <li class="poppins-light fs-14">Memberikan kemudahan dalam sewa menyewa barang.</li>
+                        <li class="poppins-light fs-14">Memberikan harga yang terjangkau dan batas waktu penyewaan yang telah disepakati.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="text-center" style="margin-top: 8rem;">
+    <h6 class="poppins-medium fs-14">Zewa@2024</h6>
+</section>
+@endsection
