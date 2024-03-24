21c9d16985835e9a300cf11872028e54a98a5fe8
Sadin21 <emdeasaddin21@gmail.com>
2024-03-24 08:04:04 +0700

    feat: add new section in detail product

--- resources/views/pages/customer-page/product/detail.blade.php
+++ resources/views/pages/customer-page/product/detail.blade.php
@@ -20,7 +20,7 @@
     <div class="row gap-4">
         <div class="col-4">
             <img src="{{ url('/assets/imgs/product/' . $product->foto) }}" class="card-img-top" style="max-height: 500px" alt="...">
-            <div class="border p-2 w-100 mt-4 d-flex gap-4">
+            <div class="border border-2 p-2 w-100 mt-4 d-flex gap-4">
                 <p class="mb-0 poppins-medium fs-14">Tanya seputar produk</p>
                 <a href="">
                     <i class="fa-brands fa-whatsapp fs-18"></i>
@@ -42,13 +42,39 @@
                     <h6 class="poppins-regular fs-14">Stok barang tersedia</h6>
                 </div>
             </div>
-            <div style="width: 80%" class="pb-4 border-bottom row">
+            <div style="width: 80%" class="pb-4 row">
                 <div class="col">
                     <h5 class="poppins-medium dark-green mt-4">Syarat & Ketentuan</h5>
                     <ul>
                         <li class="poppins-regular fs-14">Menyimpan kartu KTP saat melakukan penyewaan</li>
                         <li class="poppins-regular fs-14">DP minimal 50% dari harga sewa</li>
                     </ul>
+                </div>
+            </div>
+            <div style="width: 80%" class="pb-4 row gap-3">
+                <div class="col border border-2 rounded-0 py-4">
+                    <h5 class="poppins-medium dark-green mb-3">Pilih Paket</h5>
+                    <select class="form-select" aria-label="Default select example">
+                        {{-- <option selected>Open this select menu</option> --}}
+                        <option value="1">1 Hari - Rp. 20.000</option>
+                        <option value="1">2 Hari - Rp. 40.000</option>
+                        <option value="1">3 Hari - Rp. 60.000</option>
+                      </select>
+                </div>
+                <div class="col border border-2 rounded-0 py-4">
+                    <h5 class="poppins-medium dark-green mb-3">Tanggal Waktu Sewa</h5>
+                    <div class="input-group">
+                        <input type="date" aria-label="date" class="form-control">
+                        <input type="time" aria-label="time" class="form-control">
+                    </div>
+                </div>
+            </div>
+            <div style="width: 80%" class="row gap-3">
+                <div class="col p-0">
+                    <a href="" type="button" class="btn bg-medium-green poppins-medium text-white fs-14 w-100 rounded-0" style="background-color: #00A3A8;">Masukkan Keranjang</a>
+                </div>
+                <div class="col p-0">
+                    <a href="" type="button" class="btn bg-medium-green poppins-medium text-white fs-14 w-100 rounded-0" style="background-color: #00A3A8;">Lanjut Pembayaran</a>
                 </div>
             </div>
         </div>
